877395da685e38c64c23650123d6d1e2121e722e
Farhang <farhang.projects.2023@gmail.com>
2024-09-05 13:41:05 +0330

    new tailwindcss setups. now it's CLI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["vazirmatn", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
-
