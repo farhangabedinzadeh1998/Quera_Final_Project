72ada7350b06bbbc679ccad5b7ec3942d0b7d654
Farhang <farhang.projects.2023@gmail.com>
2024-09-12 22:30:10 +0330

    darkmode updated and restore updated

--- src/js/actions.js
+++ src/js/actions.js
@@ -83,7 +83,7 @@ const toggleComplete = (index) => {
 
 const restoreTodo = (index) => {
     const restoreTodo = completedTodos.splice(index, 1)[0];
-    completedTodos.push(restoreTodo);
+    todos.push(restoreTodo);
 
     countTodos();
     saveTodos();
@@ -133,28 +133,17 @@ const renderTodos = () => {
     todos.forEach((todo, index) => {
         const div = document.createElement("div");
         if (todo.editing) {
-            div.className = "addTask w-full border rounded-xl border-gray-200 bg-white p-4 space-y-4 shadow-md"
+            div.className = "addTask w-full border rounded-xl border-gray-200 bg-white dark:bg-blue-800 dark:border-blue-950 p-4 space-y-4 shadow-md"
             // دکمه اولویت ها اصلاح شود
             div.innerHTML = `
             <div class="flex flex-col space-y-4">
                     <div class="flex flex-col gap-2">
-    
-                        <input type="text" id="title-${index}" class="p-3 outline-none  font-bold text-lg text-gray-500" placeholder="نام تسک" value="${todo.title}">
-                    
-                        <textarea id="description-${index}" class="resize-none border outline-none transition duration-200 ease-out focus:drop-shadow-lg border-gray-300 p-3 rounded-lg font-semibold text-lg text-gray-400 truncate" placeholder="توضیحات">${todo.description}</textarea>
-    
+                        <input type="text" id="title-${index}" class="p-3 outline-none  font-bold text-lg dark:bg-blue-800 dark:text-white text-gray-500" placeholder="نام تسک" value="${todo.title}">
+                        <textarea id="description-${index}" class="resize-none border outline-none dark:bg-blue-900 dark:border-blue-950 transition duration-200 ease-out focus:drop-shadow-lg border-gray-300 p-3 rounded-lg font-semibold text-lg text-gray-400 truncate" placeholder="توضیحات">${todo.description}</textarea>
                     </div>
-
-                    <!-- <div id="dropdown" class="dropdown cursor-pointer space-y-2 shadow-md w-fit border rounded-lg transition ease-out duration-300 hover:shadow hover:scale-105">
-                        <div id="select" class="select flex flex-row-reverse space-x-3 font-bold text-base items-center justify-center px-3 py-2 text-gray-400 ">
-                            <span id="selected" class="selected pt-0.5">اولویت</span>
-                            <img id="caret" class="-rotate-90" src="./src/assets/images/main-items/add-new/tag-right.png" alt="tag">
-                        </div>
-                    </div> -->
-                     
                 </div>
     
-                <select id="priority-${index}" class="text-center border-gray-200 border bg-white w-1/6  p-4 mb-2 rounded-lg font-bold text-base items-center justify-center text-gray-600">
+                <select id="priority-${index}" class="text-center dark:bg-blue-900 dark:border-blue-950 dark:text-white border-gray-200 border bg-white w-1/6  p-4 mb-2 rounded-lg font-bold text-base items-center justify-center text-gray-600">
                 <option value="default" disabled selected>
                 اولویت ها
                 </option>
@@ -168,10 +157,10 @@ const renderTodos = () => {
                     todo.priority === "Low" ? "selected" : ""
                 }>پایین</option>
                 </select>
-                <div class="border border-gray-300"></div>
+                <div class="border border-gray-300 dark:border-blue-950"></div>
     
                 <div class="text-left pt-4 pl-2">
-                    <button class=" tranisition duration-300 hover:scale-105 hover:bg-blue-500 submit-btn bg-blue-400 rounded-xl py-1.5  px-4 font-semibold text-sm text-white">اضافه کردن تسک</button>
+                    <button class=" tranisition duration-300 hover:scale-105 dark:bg-blue-900 hover:bg-blue-500 submit-btn bg-blue-400 rounded-xl py-1.5  px-4 font-semibold text-md text-white">اضافه کردن تسک</button>
                 </div>
     
             </div>`;
@@ -180,7 +169,7 @@ const renderTodos = () => {
 
             
         } else {
-            div.className = "task flex relative bg-white border py-5 rounded-xl shadow-md"
+            div.className = "task dark:bg-blue-800 dark:border-blue-950 flex relative bg-white border py-5 rounded-xl shadow-md"
             div.innerHTML = `
             <div class="border-4 rounded-l ml-4 
             ${
@@ -194,12 +183,11 @@ const renderTodos = () => {
             <div class="flex gap-4 items-center">
                 <input type="checkbox" class="complete-checkbox border rounded-xl border-gray-300 w-6 h-6 mt-2">
                 <div class="flex flex-col gap-1">
-                    <span class="font-bold text-xl text-black">${todo.title}</span>
-                    <span class="font-semibold text-sm text-gray-500">${todo.description}</span>
+                    <span class="font-bold text-xl text-black dark:text-white">${todo.title}</span>
+                    <span class="font-semibold text-sm text-gray-500 dark:text-gray-200">${todo.description}</span>
                 </div>
             </div>
-            <!--<img class="doted-menu absolute top-5 cursor-pointer p-3 pl-1 left-5 items-start mr-auto" src="./src/assets/images/main-items/task/Frame 1000005552.png" alt="delete-edit-menu"> -->
-            <div id="delete-edit" class="flex absolute top-7 left-10  gap-2 border border-gray-300 rounded-lg bg-white p-1">
+            <div id="delete-edit" class="dark:bg-blue-400 dark:border-blue-700 flex absolute top-7 left-10  gap-2 border border-gray-300 rounded-lg bg-white p-1">
                     <img id="edit-btn" class="edit-btn cursor-pointer " src="./src/assets/images/main-items/task/tabler_edit.png" alt="edit">
                     <img id="delete-btn" class="delete-btn cursor-pointer " src="./src/assets/images/main-items/task/tabler_trash-x.png" alt="delete">
             </div>
@@ -227,16 +215,18 @@ const renderTodos = () => {
 
     completedTodos.forEach((todo, index) => {
         const div = document.createElement("div");
-        div.className = "flex flex-row bg-white border py-5 rounded-xl shadow-md";
+        div.className = "dark:bg-blue-800 dark:border-blue-950 flex flex-row bg-white border py-5 rounded-xl shadow-md";
         div.innerHTML = `
         <div class="border-4 rounded-l ml-4"></div>
                 <div class="flex gap-4 items-center">
                     <input type="checkbox" checked class="restore-checkbox border rounded-xl border-gray-300 w-6 h-6 mt-2">
-                    <span class="font-bold text-xl text-black line-through">${todo.title}</span>
+                    <span class="font-bold text-xl text-black line-through dark:text-white">${todo.title}</span>
                 </div>`;
 
         div.querySelector(".restore-checkbox").addEventListener("click", () => restoreTodo(index));
         
+
+
         completedContainer.appendChild(div);
     });
 };
